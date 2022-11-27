8bda79f47ac631c7aea5147731cdb0c28070de5c
Ahmed Ali Ansari <ahmedaliansari506@gmail.com>
2022-11-27 18:45:42 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,54 @@
+##this is the Source code of Node js and Typescript Based Calculator Runs on the Command line Interface , its package Also published at NPM Marketplace. to run calculator do :
+
+1) npm i type-script-dice-guess-game
+
+2) npx ahdgame
+
+3) initialized typescript enviroment through npx tsc -init
+
+4) intialized node js enviroment npm init -yes
+
+5) npm i chalk 
+
+6) npm i chalk-animation 
+
+7) npm i inquirer
+
+8) npm i nanospinner
+
+9) npm install typescript --save-dev
+
+10) npm i -g typescript 
+
+11) npx tsc --init
+
+12) npm i ts-node-dev
+
+13) npm install nodemon --save-dev
+
+14) npm install @types/node --save-dev
+
+15)Add .js file with this at top : #! /usr/bin/env node
+
+16)setup Json file(example) :
+
+"bin": { "multiply": "bin/index.js" },
+
+17)Test package locally(for example :
+
+npx multiply
+
+18) npm i --save-dev @types/prompt-sync
+
+19) npm i --save-dev @types/figlet
+
+20) npm i --save-dev @types/gradient-string
+
+19)create or login your npm account
+
+npm login
+
+20)publish your package
+
+21) npm publish
+
---
+++ src/index.ts
@@ -0,0 +1,77 @@
+#! /usr/bin/env node
+import figlet from "figlet";
+import chalk from "chalk";
+import inquirer from "inquirer";
+import chalkAnimation from "chalk-animation"
+import randomeInteger from "random-int";
+import gradient from 'gradient-string';
+import { createSpinner } from "nanospinner";
+import PromptSync from "prompt-sync";
+
+
+//let playerName;
+
+const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
+const sleep2 = (ms = 500) => new Promise((r) => setTimeout(r, ms));
+
+async function welcome() {
+  const rainbowTitle = chalkAnimation.rainbow(
+    'Welcome to the Dice Roll Guessing Game \n'
+  );
+  
+  await sleep();
+  rainbowTitle.stop();
+}
+await welcome();
+const answers = await inquirer.prompt({
+    name: 'player_name',
+    type: 'input',
+    message: 'What is your name?',
+    default() {
+      return 'Player';
+    },
+}
+);
+function winner() {
+    figlet(`Congrats , ${answers.player_name} !\n`, (err, data) => {
+      console.log(gradient.pastel.multiline(data) + '\n');
+    });
+       sleep();
+  }
+  var points: number;
+  points = 0;
+do{
+    var computer_guess = randomeInteger(0,6);
+    var answer = await inquirer.prompt([
+        {
+            name: "guess",
+            type: "number",
+            message: "Computer Just Rolled A dice ! whats your Guess ? "
+        },
+        // {
+        //     name : "cont",
+        //     type: "confirm",
+        //     message:"want to continue yes or no ?"
+        // },
+    ]);
+    const spinner = createSpinner('Checking answer...').start();
+    await sleep2();
+    if (!(answer.guess === computer_guess)) {
+        spinner.error({ text: `\nSorry Mate ! Your Guess is ${chalk.bgRed('Wrong'
+        )} ${answers.player_name}. \n${chalk.magenta(`Better luck Next Time`)}` });
+        // console.log(`Sorry Mate ! ${chalk.bgCyan(answers.player_name)} Your Guess is ${chalk.bgRed('Wrong'
+        // )}`);
+    }
+    else{   
+        spinner.success({ text: `\nNice work ${chalk.bgCyan(answers.player_name)}. You'r Lucky ` });
+    //    console.log(`\nNice work ${chalk.bgCyan(answers.player_name)}. You'r Lucky `);
+        points += 10;
+        console.log(chalk.bgGreen(`\nYou Have Just earned `));
+        console.log(`\nPoints :  ${chalk.yellow(points)}  🎉`);
+        console.log(`\nComputer Guess : ${chalk.blue(computer_guess)} Matched with Your Guess : ${chalk.blue(answer.guess)} `);
+        winner();
+      }
+    const prompt1 = PromptSync();
+    var check = prompt1("want to Continue y or n ? ");
+}while(!(check == 'n'))
+console.log("\nThankYou FOr playing the Game Comback Leter !");
